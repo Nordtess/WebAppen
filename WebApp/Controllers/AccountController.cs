d5d8b0861dd42d910c6097679d56cf1d2e65f810
Nordtess <nordtessj@gmail.com>
2025-12-16 17:36:03 +0100

    Jamie (#1)
    * x
    
    * x

---
+++ WebApp/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult NewUser()
+        {
+            return View();
+        }
+    }
+}
