78c00dbfe13b4b36130a6e93fccf13423e23f44c
Nordtess <nordtessj@gmail.com>
2025-12-19 21:40:10 +0100

    x

--- WebApp/Controllers/InspectProjectController.cs
+++ WebApp/Controllers/InspectProjectController.cs
@@ -2,10 +2,22 @@
 
 namespace WebApp.Controllers
 {
+    [Route("InspectProject")]
     public class InspectProjectController : Controller
     {
+        // /InspectProject
+        [HttpGet("")]
         public IActionResult Index()
         {
+            ViewData["ProjectId"] = 0;
+            return View("InspectProject");
+        }
+
+        // /InspectProject/{id}
+        [HttpGet("{id:int}")]
+        public IActionResult ById(int id)
+        {
+            ViewData["ProjectId"] = id;
             return View("InspectProject");
         }
     }
